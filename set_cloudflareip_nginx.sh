772f844b1eb116bdbe1b9be82bb2032597d006c2
Rajuthapovan <rantonysamy@thapovan-inc.com>
2017-09-04 12:33:06 +0530

    set cloudflare ip

---
+++ set_cloudflareip_nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# configure  cloudflare ips into nginx 
+
+echo "#Cloudflare" > /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+for i in `curl https://www.cloudflare.com/ips-v4`; do
+        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+done
+
+for i in `curl https://www.cloudflare.com/ips-v6`; do
+        echo "set_real_ip_from $i;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+done
+echo "real_ip_header CF-Connecting-IP;" >> /etc/nginx/conf.d/00_real_ip_cloudflare_00.conf;
+
+service nginx configtest && service nginx restart
+
+
